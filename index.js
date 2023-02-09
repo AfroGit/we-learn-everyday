d671d77d737453472d238f17c824cf9358a33180
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-02-09 22:23:40 +0000

    Applied the for.....in method to iterate through my object.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-let x = 1;
+/*Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list of crew roles and names in the following format: '[crew member's role]: [crew member's name]', e.g.,'chief officer: Dan'.   */ 
+
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() { console.log(`Jets on!`) } },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+// Write your code below
+// for (let crewMember in spaceship.crew) {
+  // console.log(` ${spaceship.crew[crewMember].name}:`);
+// }
+// console.log(``)
+
+/*Using for...in, iterate through the spaceship.crew object in the code editor and console.log() a list of crew names and degrees in the following format: '[crew member's name]: [crew member's degree]', i.e.,'Lily: Computer Engineering'.
+ */ 
+
+let crew = spaceship.crew
+for (let crewED in crew) {  
+  console.log(`${crew[crewED].name}: ${crew[crewED].degree}`)
+}
